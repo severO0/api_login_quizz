cbbda420fea4fd0b673ccc3a6861cf979db3a31b
Walace Junior <walace0712@gmail.com>
2025-03-14 12:10:17 -0300

    Some fixes

--- testeapi/src/main/java/testedeapi/com/security/JwtAuthFilter.java
+++ testeapi/src/main/java/testedeapi/com/security/JwtAuthFilter.java
@@ -6,11 +6,15 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import testedeapi.com.service.JwtService;
+
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+@Component
 @RequiredArgsConstructor
 public  class JwtAuthFilter extends OncePerRequestFilter {
     private  final JwtService jwtService;
@@ -21,12 +25,13 @@ public  class JwtAuthFilter extends OncePerRequestFilter {
                                     HttpServletResponse response,
                                     FilterChain filterChain)
             throws ServletException, IOException {
-     String requestURI = request.getRequestURI();
-
-    if (requestURI.startsWith("/api/auth") || requestURI.equals("/api/users/register")) {
-        filterChain.doFilter(request, response);
-        return;
-    }
+        String requestURI = request.getRequestURI();
+        if (requestURI.startsWith("/api/auth") || requestURI.equals("/api/users/register")) {
+             filterChain.doFilter(request, response);
+            return;
+        
+        }
+        
         String authHeader = request.getHeader("Authorization");
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
@@ -38,14 +43,15 @@ public  class JwtAuthFilter extends OncePerRequestFilter {
 
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-            if (jwtService.isTokenValid(token, userDetails)) {
-                SecurityContextHolder.getContext().setAuthentication(
-                        jwtService.getAuthentication(token, userDetails)
-                );
+
+        if (jwtService.isTokenValid(token, userDetails)) {
+            Authentication authentication = jwtService.getAuthentication(token, userDetails);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
             }
             
         }
 
         filterChain.doFilter(request, response);
     }
+    
 }
